59a83895ba3c54b770dd92993e34b16758853cdb
Fadil <fadilfdl2710@gmail.com>
2022-05-15 02:18:16 +0700

    cendol lagi

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Text("Text Edit Lagi buat cendol git hub wkwk!!"));
+        home: Text("Text Edit Lagi buat cendol git huub wkwk!!"));
   }
 }
