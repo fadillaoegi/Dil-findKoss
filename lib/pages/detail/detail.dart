51fc84217a0d99c1149f673aa1d063f0572fd45f
Fadil <fadilfdl2710@gmail.com>
2022-08-12 09:33:08 +0700

    Update InkWell

--- lib/pages/detail/detail.dart
+++ lib/pages/detail/detail.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-class detail extends StatelessWidget {
-  const detail({Key? key}) : super(key: key);
+class Detail extends StatelessWidget {
+  const Detail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(children: [
-        Text("Testing")
-      ]),
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Testing"),
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
--- lib/widgets/rekomend_card.dart
+++ lib/widgets/rekomend_card.dart
@@ -1,4 +1,5 @@
 import 'package:findkoss/models/space.dart';
+import 'package:findkoss/pages/detail/detail.dart';
 import 'package:findkoss/style/textstyle.dart';
 import 'package:flutter/material.dart';
 import '../style/style.dart';
@@ -11,73 +12,79 @@ class rekomend extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Container(
-            height: 110,
-            width: 130,
-            child: Stack(children: [
-              Image.asset(
-                spaceModel.imageUrl,
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                    height: 30,
-                    width: 70,
-                    child: Center(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                          Image.asset(
-                            "assets/images/icon_star.png",
-                            width: 22,
-                            height: 22,
-                          ),
-                          Text(
-                            spaceModel.rating.toString(),
-                            style: TsWhiteMedium.copyWith(fontSize: 13),
-                          )
-                        ])),
-                    decoration: BoxDecoration(
-                        color: colorPurple,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(30),
-                        ))),
-              )
-            ]),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Detail()));
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              height: 110,
+              width: 130,
+              child: Stack(children: [
+                Image.asset(
+                  spaceModel.imageUrl,
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                      height: 30,
+                      width: 70,
+                      child: Center(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                            Image.asset(
+                              "assets/images/icon_star.png",
+                              width: 22,
+                              height: 22,
+                            ),
+                            Text(
+                              spaceModel.rating.toString(),
+                              style: TsWhiteMedium.copyWith(fontSize: 13),
+                            )
+                          ])),
+                      decoration: BoxDecoration(
+                          color: colorPurple,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                          ))),
+                )
+              ]),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                spaceModel.name,
-                style: TsBlackMedium.copyWith(fontSize: 18),
-              ),
-              BoxH2,
-              Text.rich(
-                TextSpan(
-                    text: "\$ ${spaceModel.price} ",
-                    style: Ts4.copyWith(fontSize: 16),
-                    children: [
-                      TextSpan(
-                          text: " /mount", style: Ts3.copyWith(fontSize: 16))
-                    ]),
-              ),
-              BoxH16,
-              Text(
-                "${spaceModel.city}, ${spaceModel.country}",
-                style: Ts3.copyWith(fontSize: 14),
-              ),
-            ],
-          ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  spaceModel.name,
+                  style: TsBlackMedium.copyWith(fontSize: 18),
+                ),
+                BoxH2,
+                Text.rich(
+                  TextSpan(
+                      text: "\$ ${spaceModel.price} ",
+                      style: Ts4.copyWith(fontSize: 16),
+                      children: [
+                        TextSpan(
+                            text: " /mount", style: Ts3.copyWith(fontSize: 16))
+                      ]),
+                ),
+                BoxH16,
+                Text(
+                  "${spaceModel.city}, ${spaceModel.country}",
+                  style: Ts3.copyWith(fontSize: 14),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
