e47394ef681aa358c9c01fce39f7e99bf3ca2675
Fadil <fadilfdl2710@gmail.com>
2022-08-22 00:38:56 +0700

    Break Error

--- lib/models/space.dart
+++ lib/models/space.dart
@@ -1,12 +1,107 @@
+// To parse this JSON data, do
+//
+//     final space = spaceFromJson(jsonString);
+
+import 'dart:convert';
+
+Space spaceFromJson(String str) => Space.fromJson(json.decode(str));
+
+String spaceToJson(Space data) => json.encode(data.toJson());
+
 class Space {
+  Space({
+    required this.id,
+    required this.price,
+    required this.numberOfKitchen,
+    required this.numberOfBedRooms,
+    required this.numberOfClipBoard,
+    required this.phone,
+    required this.imageUrl,
+    required this.mapUrl,
+    required this.name,
+    required this.country,
+    required this.rating,
+    required this.city,
+  });
+
   int id;
-  String name;
-  String imageUrl;
   int price;
-  String city;
+  int numberOfKitchen;
+  int numberOfBedRooms;
+  int numberOfClipBoard;
+  String phone;
+  String imageUrl;
+  String mapUrl;
+  String name;
   String country;
   double rating;
+  String city;
 
-  Space(this.id, this.name, this.imageUrl, this.price, this.city, this.country,
-      this.rating);
+  factory Space.fromJson(Map<String, dynamic> json) => Space(
+        id: json["id"],
+        price: json["price"],
+        numberOfKitchen: json["numberOfKitchen"],
+        numberOfBedRooms: json["numberOfBedRooms"],
+        numberOfClipBoard: json["numberOfClipBoard"],
+        phone: json["phone"],
+        imageUrl: json["imageUrl"],
+        mapUrl: json["mapUrl"],
+        name: json["name"],
+        country: json["country"],
+        rating: json["rating"].toDouble(),
+        city: json["city"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "price": price,
+        "numberOfKitchen": numberOfKitchen,
+        "numberOfBedRooms": numberOfBedRooms,
+        "numberOfClipBoard": numberOfClipBoard,
+        "phone": phone,
+        "imageUrl": imageUrl,
+        "mapUrl": mapUrl,
+        "name": name,
+        "country": country,
+        "rating": rating,
+        "city": city,
+      };
 }
+
+
+
+
+// class Space {
+//   int id;
+//   int price;
+//   int numberOfKitchen;
+//   int numberOfBedrooms;
+//   int numberOfCupBoard;
+//   String name;
+//   String imageUrl;
+//   String mapUrl;
+//   String phone;
+//   String address;
+//   String city;
+//   String country;
+//   double rating;
+//   List photos;
+
+//   Space(
+//       this.id,
+//       this.name,
+//       this.imageUrl,
+//       this.price,
+//       this.city,
+//       this.country,
+//       this.rating,
+//       this.address,
+//       this.mapUrl,
+//       this.numberOfBedrooms,
+//       this.numberOfCupBoard,
+//       this.numberOfKitchen,
+//       this.phone,
+//       this.photos);
+
+//       factory Space.fromJson(Map<String, dynamic>json) => Space(, name, imageUrl, price, city, country, rating, address, mapUrl, numberOfBedrooms, numberOfCupBoard, numberOfKitchen, phone, photos)
+// }
---
+++ lib/providers/space_provider.dart
@@ -0,0 +1,10 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SpaceProvider extends ChangeNotifier {
+  getApiRecomendedSpace() async {
+    // var result = await http.get
+  }
+}
